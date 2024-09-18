2b5e55737f80dc7ff2ca2aea06ca994cc382cf8d
must_eat <mss@tutanota.de>
2024-09-18 18:40:38 +0300

    rwr: (wya/) app.py: > move flask bits to a class called WYA(), move route decorators to attribs. > get_client_ip() -> _get_client_ip(). > get_ip_info() -> _get_ip_info():  - simplify it into a single try-except.  - merge is_bogon() behavior into the try. > add _signal_handler() and _set_signal_handling(); gracefully die on TERM and   INT, reload GeoLite2 db's on HUP. > run():  - set the root logger and create your own logger here.  - set signal handling, create an IPQuery object and call serve.  - give the TransLogger its own logger called "access".  - set the port to 8080 hard.

--- wya/app.py
+++ wya/app.py
@@ -1,60 +1,91 @@
 import ipaddress
 import logging
+import signal
+import sys
 
 from flask import Flask, jsonify, request
-from waitress import serve
 from paste.translogger import TransLogger
+from waitress import serve
 
-from .log import set_root_logger
-from .geoquery import GeoQuery
 from . import __version__ as pkg_version
+from .ipquery import IPQuery
+from .log import set_root_logger
 
-set_root_logger(debug=True)
-logger = logging.getLogger("wya")
 
-gquery = GeoQuery()
+class WYA:
+    def __init__(self):
+        self.app = Flask("wya")
+        self.app.json.sort_keys = False
+        self.app.route("/", methods=["GET"])(self._get_client_ip)
+        self.app.route("/<ip_address>", methods=["GET"])(self._get_ip_info)
 
-app = Flask("wya")
-app.json.sort_keys = False
+        self.logger = None
+        self.ipquery = None
 
-def is_bogon(ip):
-    try:
-        ip_obj = ipaddress.ip_address(ip)
-        return (
-            ip_obj.is_private
-            or ip_obj.is_loopback
-            or ip_obj.is_link_local
-            or ip_obj.is_multicast
-            or ip_obj.is_reserved
+    def _get_client_ip(self):
+        if request.headers.get("X-Real-IP"):
+            ip_address = request.headers.get("X-Real-IP")
+        elif request.headers.get("X-Forwarded-For"):
+            ip_address = request.headers.get("X-Forwarded-For").split(",")[0].strip()
+        else:
+            ip_address = request.remote_addr
+
+        return self._get_ip_info(ip_address)
+
+    def _get_ip_info(self, ip_address):
+        try:
+            ip_obj = ipaddress.ip_address(ip_address)
+
+            if (
+                ip_obj.is_private
+                or ip_obj.is_loopback
+                or ip_obj.is_link_local
+                or ip_obj.is_multicast
+                or ip_obj.is_reserved
+            ):
+                return jsonify({"error": "invalid public ip address"}), 400
+        except ValueError:
+            return jsonify({"error": "invalid ip address"}), 400
+
+        return jsonify(self.ipquery.mkdict(self.ipquery.query(ip_address)))
+
+    def _signal_handler(self, signum, frame):  # pylint: disable=unused-argument
+        signame = signal.Signals(signum).name
+
+        if signame in ("SIGINT", "SIGTERM"):
+            self.logger.info("caught %s, initiating graceful shutdown", signame)
+            sys.exit(0)
+
+        if signame in ("SIGHUP"):
+            self.logger.info("caught SIGHUP, reloading GeoLite2 db's")
+            self.ipquery.load_dbs()
+
+    def _set_signal_handling(self):
+        self.logger.info("set signal handling")
+        signal.signal(signal.SIGINT, self._signal_handler)
+        signal.signal(signal.SIGTERM, self._signal_handler)
+        signal.signal(signal.SIGHUP, self._signal_handler)
+
+    def run(self):
+        set_root_logger()
+
+        self.logger = logging.getLogger("wya")
+        self.logger.info("started wya ver. %s", pkg_version)
+
+        self._set_signal_handling()
+
+        self.ipquery = IPQuery()
+
+        access_logger = logging.getLogger("access")
+
+        serve(
+            TransLogger(self.app, logger=access_logger),
+            host="0.0.0.0",
+            port=8080,
+            threads=32,
         )
-    except ValueError:
-        return True
-
-@app.route("/", methods=["GET"])
-def get_client_ip():
-    if request.headers.get("X-Real-IP"):
-        ip_address = request.headers.get("X-Real-IP")
-    elif request.headers.get("X-Forwarded-For"):
-        ip_address = request.headers.get("X-Forwarded-For").split(",")[0].strip()
-    else:
-        ip_address = request.remote_addr
-
-    return get_ip_info(ip_address)
-
-
-@app.route("/<ip_address>", methods=["GET"])
-def get_ip_info(ip_address):
-    try:
-        ipaddress.ip_address(ip_address)
-    except ValueError:
-        return jsonify({"error": "invalid IPv4 address"}), 400
-
-    if is_bogon(ip_address):
-        return jsonify({"error": "bogon"}), 400
-
-    return jsonify(gquery.geo_to_dict(gquery.geoquery(ip_address)))
 
 
 def run():
-    logger.info("started wya ver. %s", pkg_version)
-    serve(TransLogger(app), host='0.0.0.0', port = 6666, threads = 32)
+    a = WYA()
+    a.run()
